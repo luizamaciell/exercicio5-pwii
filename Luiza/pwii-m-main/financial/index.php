14cf4e8676b6b0e958ff18325013b46345af1694
Luiza Maciel <110676520+luizamaciell@users.noreply.github.com>
2023-04-14 08:07:34 -0300

    Update index.php

--- Luiza/pwii-m-main/financial/index.php
+++ Luiza/pwii-m-main/financial/index.php
@@ -16,14 +16,17 @@ var_dump($address);
 $user = new User("Luiza", "Luizamaciel@gmail.com", "1234", "04456320050", "140507", $address);
 var_dump($user);
 
-$Saving01 = new saving("1255", "5000.0000", "09/09");
-$Saving02 = new saving("4444", "40.000", "08/08");
+$client = new client("Luiza", "Luizamaciel@gmail.com", "1234", "04456320050", "140507", $address, "10.000", "TI");
+var_dump($client);
+
+$saving01 = new saving("1255", "5000.0000", "09/09");
+$saving02 = new saving("4444", "40.000", "08/08");
 $client->addSaving($saving01);
 $client->addSaving($saving02);
 
 //var_dump($Saving);
 //$Employee = new employee();
-//$Client = new client();
+
 //$Employee = new employee();
 //$Current = new  current();
 //$Products = new Products();
